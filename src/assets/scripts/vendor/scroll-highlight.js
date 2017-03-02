3d0017e3aba42741a3411c87ff0d99c601f8808d
Mitchell Warren <hello@liquidvisual.com>
2017-03-02 13:48:03 +1100

    Scroll highlight implemented

--- src/assets/scripts/vendor/scroll-highlight.js
+++ src/assets/scripts/vendor/scroll-highlight.js
@@ -1,18 +1,85 @@
 /*
-    SCROLL HIGHLIGHT - Last updated: 01.03.17
+    SCROLL-HIGHLIGHT.JS - Last updated: 02.03.17
 
-    - Notes -
-
-     An alternative to Foundation's Magellan:
-     http://jsfiddle.net/mekwall/up4nu/
+    - Notes: http://jsfiddle.net/mekwall/up4nu/
 */
 //-----------------------------------------------------------------
-// VARIABLES
+//
 //-----------------------------------------------------------------
 
+;(function($) {
+    'use strict';
 
+    //-----------------------------------------------------------------
+    // VARIABLES
+    //-----------------------------------------------------------------
 
+	var lastId;
+	var topMenu = $('[data-scroll-highlight]');
+	var topMenuHeight = topMenu.outerHeight();
+	var menuItems = topMenu.find("a");
 
+	//-----------------------------------------------------------------
+	// MAP MENU TARGET
+	//-----------------------------------------------------------------
+
+	var scrollItems = menuItems.map(function(){
+		if ($(this).attr('href') == '#')
+			return null;
+			var item = $($(this).attr('href'));
+			if (item.length) return item;
+		});
+
+	 //-----------------------------------------------------------------
+	 // CLICK
+	 //-----------------------------------------------------------------
+
+	 menuItems.click(function(e){
+	   var href = $(this).attr("href"),
+	       o = href === "#" ? 0 : $(href).offset().top-topMenuHeight+15;
+	   $('html, body').stop().animate({
+	       scrollTop: o
+	   }, 800);
+	   e.preventDefault();
+	 });
+
+	 //-----------------------------------------------------------------
+	 // BIND SCROLL
+	 //-----------------------------------------------------------------
+
+	$(window).scroll(function(){
+
+		var fromTop = $(this).scrollTop()+topMenuHeight;
+
+		// Get id of current scroll item
+		var cur = scrollItems.map(function(){
+		if ($(this).offset().top < fromTop)
+			return this;
+		});
+
+		// Get the id of the current element
+		cur = cur[cur.length-1];
+
+		var id = cur && cur.length ? cur[0].id : "";
+
+		if (lastId !== id) {
+			lastId = id;
+			menuItems
+			.parent().removeClass('active')
+			.end().filter('[href="#'+id+'"]').parent().addClass('active');
+		}
+
+		// Bit hacky - activate on scroll bottom
+		if ($(window).scrollTop() + $(window).height() == $(document).height()) {
+			menuItems.parent().removeClass('active').find('[href="#contact-us"]').parent().addClass('active');
+		}
+	});
+//--
+}(jQuery));
+
+//==================================================
+//
+//==================================================
 //==================================================
 //
 //==================================================
